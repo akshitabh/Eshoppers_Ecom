2a3e9c7805a81a14c6db7fefb9318fd67ca57dbc
akshitabh <akshita9754@gmail.com>
2023-05-23 18:08:58 +0530

    'shoppingweb'

---
+++ Ecom/app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib import admin
+from . models import *
+
+# Register your models here.
+admin.site.register(Customer),
+admin.site.register(Product),
+admin.site.register(Cart),
+admin.site.register(OrderPlaced),
+# Register your models here.
